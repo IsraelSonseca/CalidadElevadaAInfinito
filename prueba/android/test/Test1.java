1d4711d806371a358b8233438622c84aa53b35a3
daolao <dalao1002@gmail.com>
2019-02-18 14:01:50 +0100

    make app run

--- prueba/android/test/Test1.java
+++
@@ -1,9 +0,0 @@
-import org.junit.Test;
-
-public class Test1 {
-
-        @Test
-        public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
-            System.out.println("Hola");
-        }
-}
